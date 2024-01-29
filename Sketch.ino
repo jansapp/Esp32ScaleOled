1eb506a007db2b999abdc8f8b47f530016fa2f1e
jansapp <60516840+jansapp@users.noreply.github.com>
2024-01-29 20:24:08 +0100

    Create Sketch.ino

---
+++ Sketch.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "HX711.h"
+
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
+const int GAIN = 128; //Folgende Verstärkungen sind möglich: 32, 64, 128
+const float MESSWERTE = 1; //Anzahl der Messwerte (wirkt als gleitender Mittelwert / Hochpass)
+const float FAKTOR = 420; //Skalierung ADC Wert zu Kraft (ADC Wert / Kraft)
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize OLED display with I2C address 0x3C
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("failed to start SSD1306 OLED"));
+    while (1);
+  }
+  scale.begin(0, 1);
+  scale.set_gain(GAIN); //Verstärkung
+  scale.set_scale(FAKTOR); //Skalierung (konvertiert ADC Wert in Newton)
+
+  delay(2000);
+  oled.clearDisplay();
+
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.setCursor(0, 10);
+  oled.println("JanWarHier");
+  oled.display();            
+  Serial.println(F("SetupDone"));
+}
+
+void loop() {  
+  
+  delay(2000);
+  oled.clearDisplay(); // clear display
+
+  oled.setTextSize(1);        
+  oled.setTextColor(WHITE);   
+  oled.setCursor(0, 10);      
+  Serial.print(scale.get_units(),5);
+  Serial.println(" Kg");
+  oled.print(scale.get_units(),5);
+  oled.println("Kg");
+  oled.display();   
+}
